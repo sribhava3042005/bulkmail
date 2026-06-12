56713953b35e9367d6ba4316f546faf4e9af7672
Sri Bavadharani <sribavadharani201@gmail.com>
2026-06-12 08:14:48 -0700

    bulk mail backend

---
+++ demo.js
@@ -0,0 +1,21 @@
+const fileInput = document.getElementById('fileInput');
+
+fileInput.addEventListener('change', (event) => {
+const file = event.target.files[0]
+console.log(file)
+
+
+
+const reader = new FileReader();
+reader.onload = function (e) {
+const data = e.target.result;
+const workbook = XLSX.read(data, { type: 'binary' })
+const sheetName = workbook.SheetNames[0]
+const worksheet = workbook.Sheets [sheetName]
+const emaillist = XLSX.utils.sheet_to_json(worksheet, {header: 'A'})
+
+console.log(emailList)
+
+reader.readAsBinaryString(file);
+
+});
---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express")
+const cors = require("cors")
+const nodemailer = require("nodemailer");
+const mongoose = require("mongoose")
+const app = express()
+
+app.use (cors())
+app.use (express.json())
+
+mongoose.connect("mongodb+srv://sribhava:anu3042005@cluster0.y11kodt.mongodb.net/passkey?appName=Cluster0").then(function(){
+  console.log("connected to DB")
+})
+
+const credential = mongoose.model("credential", {},"bulkmail")
+
+credential.find().then(function(data){
+
+console.log(data[0].toJSON())
+});
+//Install NODEMAILER
+
+const transporter = nodemailer.createTransport({
+    service:"gmail",
+    auth: {
+        // TODO: replace 'user' and 'pass' values from <https://forwardemail.net>
+        user: "sribavadharani201@gmail.com",
+        pass: "kqxx cpho ppfj hjvx",
+    },
+});
+app.post("/sendmail",function(req,res){
+    var msg = req.body.msg;
+    var emaillist = req.body.emaillist;
+
+    for(var i=0;i<emaillist.length;i++)
+    {
+       transporter.sendMail({
+    from:"sribavadharani201@gmail.com",
+    to:emaillist[i],
+    subject: "A message from bulkmail app",
+    text:msg
+       
+},
+    
+ 
+
+function(error,info){
+    if(error)
+    {
+     console.log(error)
+     res.send(false)
+    }
+    else{
+     console.log(info);
+    }
+});
+    }
+     res.send(true)  
+    
+});
+
+
+
+
+app.listen(5000,function(){
+    console.log("Server Started.......");
+});

b417a42e84032b5de7254e552ffe829e8029cd8b
Sri Bavadharani <sribavadharani201@gmail.com>
2026-06-16 05:17:24 -0700

    fix app.js build error

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -25,15 +25,16 @@ reader.onload = function (e) {
 const data = e.target.result;
 const workbook = XLSX.read(data, { type: 'binary' })
 const sheetName = workbook.SheetNames[0]
-const worksheet = workbook.Sheets [sheetName]
-const emaillist = XLSX.utils.sheet_to_json(worksheet, {header: 'A'})
-const totalemail = emaillist.map(function(item){return item.A})
+const worksheet = workbook.Sheets[sheetName];
+const emaillist = XLSX.utils.sheet_to_json(worksheet, {header:'A'});
+const totalemail = emaillist.map(function(item){return item.A});
 console.log(totalemail)
 setEmailist(totalemail)
+}
 
 reader.readAsBinaryString(file);
 
-}
+
   }
   function Send()
   {
